76af2281591c010abfe685383a2ac44a507cdde3
Moneef Amri <moneefamri@gmail.com>
2023-11-28 23:25:15 +0300

    Push all files

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pythons3
+def upper(c):
+    if 97 <= ord(c) <= 122:
+        return ord(c) - 32
+    else:
+        return ord(c)
+
+
+def uppercase(str):
+    new = ""
+    for character in str:
+        new += "%c" % upper(character)
+    print("{:s}".format(new))
