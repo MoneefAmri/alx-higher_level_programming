f3874299a2e96bcafdeb1d034638b8ac8425d2a9
Moneef Amri <moneefamri@gmail.com>
2023-12-03 23:39:59 +0300

    List Management

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     list_len = len(my_list) - 1
     if idx < 0 or idx > list_len:
         return my_list
