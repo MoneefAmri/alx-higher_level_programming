0df2c638ebc4d7f693d4d9294bc3711fef5af76f
Moneef Amri <moneefamri@gmail.com>
2023-12-03 23:39:59 +0300

    Fix Errors

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     if my_list:
-        for num in my_list[::-1]:
-            print('{:d}'.format(num))
+        my_list.reverse()
+        for num in my_list:
+            print("{:d}".format(num))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > list_len:
         return my_list
     else:
-        new_list = my_list.copy()
+        new_list = my_list[:]
         new_list[idx] = element
         return new_list
