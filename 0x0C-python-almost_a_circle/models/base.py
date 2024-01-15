eee8f4240e629487ef8fc1dcc9aa3893c2c3ac56
Moneef Amri <moneefamri@gmail.com>
2024-01-15 23:27:04 +0300

    18. Dictionary to Instance Task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -71,3 +71,22 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set.
+        Args:
+            **dictionary: Double pointer.
+        Returns:
+            object: Instance with attributes set based on the dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+
+        dummy.update(**dictionary)
+        return dummy
