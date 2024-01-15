f6aff9e648af5ea88528df32725898952c9cb62d
Moneef Amri <moneefamri@gmail.com>
2024-01-15 22:37:19 +0300

    11. Square size Task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -48,13 +48,8 @@ class Square(Rectangle):
             TypeError: If the value is not an integer.
             ValueError: If the value is less than or equal to 0.
         """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value <= 0:
-            raise ValueError("size must be > 0")
-        else:
-            self.width = value
-            self.height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """
