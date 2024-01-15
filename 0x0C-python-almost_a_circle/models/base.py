0379abfed48b95660aff37c4eb0df3aca5513c0a
Moneef Amri <moneefamri@gmail.com>
2024-01-15 20:52:14 +0300

    1. Base class Task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module class Base"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Constructor for initializing the instance id values.
+        Attributes:
+            id (int): The id of the object.
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
